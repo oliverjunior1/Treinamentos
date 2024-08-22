b090b599d4995f65e87d4256de67c8357ac3f7ae
Joaquim <oliver-junior@hotmail.com>
2024-08-22 19:49:09 -0300

    Create creating_arrays.py

---
+++ Numpy/creating_arrays.py
@@ -0,0 +1,37 @@
+# import numpy as np
+#
+# x = np.array([1,2,3,4,5])
+#
+# print(x)
+################################################
+# import numpy as np
+#
+# x = np.array([1])
+#
+# print(x)
+################################################
+# import numpy as np
+#
+# x = np.array([[1,2,3],[4,5,6]])
+#
+# print(x)
+################################################
+# import numpy as np
+#
+# x = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+#
+# print(x)
+################################################
+import numpy as np
+
+x = np.array([1])
+y = np.array([1,2,3,4,5])
+z = np.array([[1,2,3],[4,5,6]])
+w = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+
+print(x.ndim)
+print(y.ndim)
+print(z.ndim)
+print(w.ndim)
+
+
