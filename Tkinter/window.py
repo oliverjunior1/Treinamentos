f78ee9dd5ea12fa7e8dd6d4e8dd64cf58a892a0e
Joaquim <oliver-junior@hotmail.com>
2024-08-22 22:03:33 -0300

    Create window.py

---
+++ Tkinter/window.py
@@ -0,0 +1,21 @@
+# import tkinter as tk
+#
+# root = tk.Tk()
+#
+# root.mainloop()
+################################################
+
+# import tkinter as tk
+#
+# root = tk.Tk()
+# root.title('Tkinter Window Demo')
+#
+# root.mainloop()
+################################################
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Window Title Demo')
+root.geometry('600x400')
+
+root.mainloop()
