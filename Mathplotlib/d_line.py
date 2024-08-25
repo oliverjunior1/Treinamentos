02ed80074637a78ebea6ac13a3faa140ad8e9035
Joaquim <oliver-junior@hotmail.com>
2024-08-25 13:06:07 -0300

    Create d_line.py

---
+++ Mathplotlib/d_line.py
@@ -0,0 +1,40 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# y = np.array([3,8,1,10])
+#
+# plt.plot(y, linestyle='dotted')
+# plt.show()
+##########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# y = np.array([3,8,1,10])
+#
+# plt.plot(y, linestyle='dashed')
+# plt.show()
+##########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# y = np.array([3,8,1,10])
+#
+# plt.plot(y, ls=':')
+# plt.show()
+##########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# y = np.array([3,8,1,10])
+#
+# plt.plot(y, color='r')
+# plt.show()
+##########################################
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# y = np.array([3,8,1,10])
+#
+# plt.plot(y, linewidth='20.5')
+# plt.show()
+##########################################
