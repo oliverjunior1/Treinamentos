153f048f686d6884b13067d9c33726e8ec4fb405
Joaquim <oliver-junior@hotmail.com>
2024-08-27 22:33:30 -0300

    Create e_analize_data.py

---
+++ Pandas/e_analize_data.py
@@ -0,0 +1,12 @@
+# import pandas as pd
+#
+# df = pd.read_csv('Pokemon.csv')
+#
+# print(df.head(10))
+##########################################
+import pandas as pd
+
+x = pd.read_csv('Pokemon.csv')
+
+print(x.head())
+
