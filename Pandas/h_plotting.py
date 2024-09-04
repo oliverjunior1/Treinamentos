ba9916ad642f11e54f584ea3a7473cf1b175fb82
Joaquim <oliver-junior@hotmail.com>
2024-09-04 17:02:54 -0300

    Update h_plotting.py

--- Pandas/h_plotting.py
+++ Pandas/h_plotting.py
@@ -1,8 +1,17 @@
+# import pandas as pd
+# import matplotlib.pyplot as plt
+#
+# df = pd.read_csv('data1.csv')
+#
+# df.plot()
+#
+# plt.show()
+#####################################
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data1.csv')
+df = pd.read_csv('data.csv')
 
-df.plot()
+df.plot(kind='scatter', x='Duration', y='Calories')
 
 plt.show()
