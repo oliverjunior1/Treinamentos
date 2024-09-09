187cce8bfdbe13b908f108c0b447b1bf2f8a78c2
Joaquim <oliver-junior@hotmail.com>
2024-09-08 23:39:40 -0300

    Create l_drawing_objects_and_shapes.py

---
+++ Pygame/l_drawing_objects_and_shapes.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+window = pygame.display.set_mode((600,600))
+window.fill((255,255,255))
+
+pygame.draw.rect(window,(0,0,255),[100,100,400,100],2)
+pygame.display.update()
