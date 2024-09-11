01f1b33aee42b5b02ea7f0bff9e629317244f47e
Joaquim <oliver-junior@hotmail.com>
2024-09-10 23:13:02 -0300

    Update trainners.py

--- Matplotlib/trainners.py
+++ Matplotlib/trainners.py
@@ -1,8 +1,19 @@
+#----------------pyplot----------------
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# xpoints = np.array([0,6])
+# ypoints = np.array([0,250])
+#
+# plt.plot(xpoints, ypoints)
+# plt.show()
+########################################
+#------------------ploting----------------
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0,6])
-ypoints = np.array([0,250])
+xpoints = np.array([1,8])
+ypoints = np.array([3,10])
 
 plt.plot(xpoints, ypoints)
 plt.show()
