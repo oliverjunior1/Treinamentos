0dc17fa25d951258ba2bbfee395741e7e72ee5bb
oliverjunior1 <oliver-junior@hotmail.com>
2024-09-12 21:52:04 -0300

    Update trainners.py

--- Matplotlib/trainners.py
+++ Matplotlib/trainners.py
@@ -27,10 +27,24 @@
 # plt.plot(y, marker = 'o')
 # plt.show()
 #------------------Line---------------------
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# y = np.array([3,8,1,18])
+#
+# plt.plot(y, linestyle = 'dotted')
+#
+# plt.show()
+#-------------------Labels------------------
 import numpy as np
+import matplotlib.pyplot as plt
 
-y = np.array([3,8,1,18])
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.plot(y, linestyle = 'dotted')
+plt.plot(x, y)
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
 plt.show()
