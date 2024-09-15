3167595031eafadae19f0ae700d5d3b101d83f57
oliverjunior1 <oliver-junior@hotmail.com>
2024-09-14 23:53:29 -0300

    Create q_event_handling.py

---
+++ Pygame/q_event_handling.py
@@ -0,0 +1,22 @@
+import pygame
+pygame.init()
+
+gameWindow = pygame.display.set_mode((800,300))
+pygame.display.set_caption("Event Handling")
+
+exit_game = False
+game_over = False
+
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                print("You have pressed right arrow key")
+            if event.key == pygame.K_LEFT:
+                print("You have pressed left arrow key")
+
+pygame.quit()
+quit()
