237b356a5be6dae92db098e10b68d5a8d9a71813
oliverjunior1 <oliver-junior@hotmail.com>
2024-09-14 23:39:17 -0300

    Update trainners.py

--- Matplotlib/trainners.py
+++ Matplotlib/trainners.py
@@ -36,15 +36,31 @@
 #
 # plt.show()
 #-------------------Labels------------------
-import numpy as np
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+#
+# plt.plot(x, y)
+#
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+#
+# plt.show()
+#---------------------------------------Grid------------------------------
 import matplotlib.pyplot as plt
+import numpy as np
 
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.plot(x, y)
-
+plt.title("Sports Watch Data")
 plt.xlabel("Average Pulse")
 plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid()
 
 plt.show()
