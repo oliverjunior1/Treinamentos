b52458e1efb9652efd5f8f949ccc47ed93d6d40a
oliverjunior1 <oliver-junior@hotmail.com>
2024-09-17 20:53:08 -0300

    Update trainning.py

--- Pandas/trainning.py
+++ Pandas/trainning.py
@@ -5,22 +5,22 @@
 #
 # print(a)
 #----------------------------------------
-import pandas as pd
-
-a = [1, 7, 2]
-
-myvar = pd.Series(a, index=['x', 'y', 'z'])
-
-print(myvar)
+# import pandas as pd
+#
+# a = [1, 7, 2]
+#
+# myvar = pd.Series(a, index=['x', 'y', 'z'])
+#
+# print(myvar)
 
 #-----------------------------------------
-import pandas as pd
-
-calories = {'day1':420, 'day2':380, 'day3': 390}
-
-myvar = pd.Series(calories)
-
-print(myvar)
+# import pandas as pd
+#
+# calories = {'day1':420, 'day2':380, 'day3': 390}
+#
+# myvar = pd.Series(calories)
+#
+# print(myvar)
 #--------------dataframes----------------
 # import pandas as pd
 #
