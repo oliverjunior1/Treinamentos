77d3acffbcb7f6a047886431b03a7f1a194bb307
oliverjunior1 <oliver-junior@hotmail.com>
2024-09-19 17:40:02 -0300

    Update trainners.py

--- Matplotlib/trainners.py
+++ Matplotlib/trainners.py
@@ -65,20 +65,28 @@
 #
 # plt.show()
 #-----------------------Subplot------------------------------------------------
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# x = np.array([0,1,2,3])
+# y = np.array([3,8,1,10])
+#
+# plt.subplot(1,2,1)
+# plt.plot(x, y)
+#
+# x = np.array([0,1,2,3])
+# y = np.array([10,20,30,40])
+#
+# plt.subplot(1,2,2)
+# plt.plot(x, y)
+#
+# plt.show()
+#------------------------Scatter----------------------------------------
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([0,1,2,3])
-y = np.array([3,8,1,10])
+x = np.array([1,2,3,5,4,8,9,10,15,11,12])
+y = np.array([20,22,21,25,21,24,26,27,28,29,30])
 
-plt.subplot(1,2,1)
-plt.plot(x, y)
-
-x = np.array([0,1,2,3])
-y = np.array([10,20,30,40])
-
-plt.subplot(1,2,2)
-plt.plot(x, y)
-
+plt.scatter(x, y)
 plt.show()
-
