18a2efe7bf7e492e7866ae8c6d168f81d164f208
oliverjunior1 <oliver-junior@hotmail.com>
2024-09-27 22:58:35 -0300

    Create Lists.py

---
+++ Python/Jose_Salvatierra_and_Rob_Percival/Lists.py
@@ -0,0 +1,15 @@
+# friends = ["Ralf", "Bob", "Anne"]
+#
+# print(friends)
+# print(len(friends))
+
+friends = [
+    ["Ralf", 24],
+    ["Bob", 30],
+    ["Anne", 27],
+    ["Charlie", 60]
+]
+
+print(friends[0][0])
+print(friends[1][1])
+print(friends)
