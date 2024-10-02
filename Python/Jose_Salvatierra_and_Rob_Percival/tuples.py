fc6a13aa5b2ab0bf8cbb3170c94ae951a9b400d5
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-02 19:41:06 -0300

    Update tuples.py

---
+++ Python/Jose_Salvatierra_and_Rob_Percival/tuples.py
@@ -0,0 +1,10 @@
+# short_tuple = "Ralf", "Bob"
+# a_bit_clearer = ("Ralf", "Bob")
+# tuple_in_list = [("Ralf", "Bob")]
+# not_a_tuple = "Ralf"
+
+friends = ("Ralf", "Bob", "Anne")
+# friends.append("Jen")
+friends = friends + ("Jen",)
+
+print(friends)
