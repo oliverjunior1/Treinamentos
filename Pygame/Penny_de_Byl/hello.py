3ea60ed781918aacb7a886d6b0426cf74b758d00
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-06 23:29:41 -0300

    Delete hello.py

--- Pygame/Penny_de_Byl/hello.py
+++
@@ -1,9 +0,0 @@
-import pygame
-pygame.init()
-
-screen = pygame.display.set_mode((640,840),0,32)
-pygame.display.set_caption("Hello Pygame")
-screen.fill((0,0,0))
-
-pygame.display.update()
-pygame.quit()
