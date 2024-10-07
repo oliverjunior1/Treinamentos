9ed1d8474d6812bd89ea8765d4036fbdc242bec8
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-06 23:30:00 -0300

    Create main_game_loop.py

---
+++ Pygame/Penny_de_Byl/Part I/main_game_loop.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((620,480),0,32)
+pygame.display.set_caption('Hello Pygame')
+screen.fill((0,0,0))
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+pygame.quit()
+
