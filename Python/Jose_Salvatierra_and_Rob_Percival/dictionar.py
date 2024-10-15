79e615ebcc936a21362e7af1557df1e5520b76fc
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-15 20:22:06 -0300

    Create dictionar.py

---
+++ Python/Jose_Salvatierra_and_Rob_Percival/dictionar.py
@@ -0,0 +1,25 @@
+# friend_ages = {"Ralf": 24, "Adam":30, "Anne": 27}
+#
+# friend_ages["Bob"] = 20
+# friend_ages["Ralf"]=25
+#
+# print(friend_ages)
+
+#---------------------------------------------------------
+# friends = (
+#     {"name": "Ralf Smith", "age": 24},
+#     {"name": "Adam Wool", "age": 30},
+#     {"name": "Anne Pun", "age": 27}
+# )
+#
+# # friend = friends[0]
+#
+# print(friends[0])
+# print(friends[1])
+# print(friends[2])
+#-----------------------------------------------------------
+friend_ages = [("Ralf", 24), ("Adam",30), ("Anne", 27)]
+
+friend_ages = dict(friend_ages)
+print(friend_ages)
+
