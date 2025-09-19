98269aa43b3d3b71c90a2c5d8128f8599091efec
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-18 21:36:08 -0300

    Create a_stock_control_system.py

---
+++ Python/treinamentos/b_exercicios_classes/a_stock_control_system.py
@@ -0,0 +1,10 @@
+"""🧠 Exercício: Sistema de Controle de Estoque
+Crie uma classe chamada  que represente um item em um estoque. Cada produto deve ter:
+• 	 (string)
+• 	 (float)
+• 	 (int)
+🧰 Requisitos:
+1. 	Um método  que imprime as informações do produto.
+2. 	Um método  que aumenta a quantidade.
+3. 	Um método  que diminui a quantidade, mas não permite que fique negativo.
+4. 	Um método  que retorna o valor total em estoque (preço × quantidade)."""
