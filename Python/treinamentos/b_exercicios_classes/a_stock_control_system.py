ca16abdfd011eb5d24a91c85ff6ebf2e8344511a
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-18 21:41:36 -0300

    Update a_stock_control_system.py

--- Python/treinamentos/b_exercicios_classes/a_stock_control_system.py
+++ Python/treinamentos/b_exercicios_classes/a_stock_control_system.py
@@ -8,3 +8,5 @@ Crie uma classe chamada  que represente um item em um estoque. Cada produto deve
 2. 	Um método  que aumenta a quantidade.
 3. 	Um método  que diminui a quantidade, mas não permite que fique negativo.
 4. 	Um método  que retorna o valor total em estoque (preço × quantidade)."""
+
+class estoque:
