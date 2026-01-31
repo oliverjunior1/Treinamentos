cf910986da3a16c6bc6aae29d91b2ef2e520da0a
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-30 22:46:18 -0300

    Update 3_mega_e_loto.py

--- Python/treinamentos/3_mega_e_loto.py
+++ Python/treinamentos/3_mega_e_loto.py
@@ -1,21 +1,11 @@
 import random
 
+
 def mega():
     x = sorted(random.sample(range(1,61),6))
-    print(x)
+    return x
 def lotofacil():
     x = sorted(random.sample(range(1,26),15))
-    print(x)
+    return x
 
-while True:
-    option = int(input("Put (1) to megasena, (2) to lotofacil, (3) to exit:"))
-    match option:
-        case 1:
-            mega()
-        case 2:
-            lotofacil()
-        case 3:
-            break
-        case _:
-            print("Invalid option.")
-
+print(lotofacil())
