505449e04fd26dbd791bafdb0360aa2366b806a3
oliverjunior1 <oliver-junior@hotmail.com>
2026-01-30 22:36:47 -0300

    Update a_class_.py

---
+++ Python/treinamentos/a_class_.py
@@ -0,0 +1,13 @@
+class Family:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"The name is {self.name} and the age is {self.age} years old."
+
+son = Family('Joao', 12)
+daughter = Family('Mariane', 4)
+
+print(son)
+print(daughter)
