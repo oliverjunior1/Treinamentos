2d5b29b1c7e66b91d9bcaf66dd72d4b7034da81a
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-02 19:18:57 -0300

    Update 6_zip.py

--- Python/treinamentos/6_zip.py
+++ Python/treinamentos/6_zip.py
@@ -1,7 +1,5 @@
 list_1 = ['name', 'age', 'profession']
 list_2 = ['Pepe', 12, 'gamer']
-dict_union = {}
-for x, y in zip(list_1, list_2):
-    dict_union[x] = y
+dict_union = dict(zip(list_1, list_2))
 
 print(dict_union)
