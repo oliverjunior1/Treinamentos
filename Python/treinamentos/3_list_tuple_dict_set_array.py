010f409246537e8617210025f26d0b4c8703372e
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-08 14:48:13 -0300

    Update 3_list_tuple_dict_set_array.py

--- Python/treinamentos/3_list_tuple_dict_set_array.py
+++ Python/treinamentos/3_list_tuple_dict_set_array.py
@@ -6,11 +6,11 @@ print(type(a))
 b = (1,2,3)
 print(type(b))
 
-c = {'a':1, 'b':2, 'c':3}
+c = {'a':1, 'b':2, 'c':4}
 print(type(c))
 
 d = {1,2,3}
 print(type(d))
 
-e = pd.array([1,2,3])
+e = pd.array([1,2,3,4])
 print(type(e))
