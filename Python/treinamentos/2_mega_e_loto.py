a0ce5b5673535f0de65bcc992d6b232607a1a19d
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-09 18:06:10 -0300

    Update 2_mega_e_loto.py

--- Python/treinamentos/2_mega_e_loto.py
+++ Python/treinamentos/2_mega_e_loto.py
@@ -1,3 +1,24 @@
+import random
 
+
+def mega():
+    x = list(sorted(random.sample(range(1,61),6)))
+    print(x)
+
+def facil():
+    x = list(sorted(random.sample(range(1,26),15)))
+    print(x)
+
+while True:
+    option = int(input("Put 1 to mega, 2 to lotofacil, and 3 to exit: "))
+    match option:
+        case 1:
+            mega()
+        case 2:
+            facil()
+        case 3:
+            break
+        case _:
+            print("Not a valid number.")
 
 
