05ac48cf3da6825dd780ff381e98ad047d321993
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-12 19:16:54 -0300

    Update 20_open_document_and_edit.py

--- Python/treinamentos/20_open_document_and_edit.py
+++ Python/treinamentos/20_open_document_and_edit.py
@@ -1,5 +1,5 @@
-with open('archive.txt', 'w') as archive:
-    archive.write("Jesus is the light of the world!")
+with open('archive.txt', 'w') as arquive:
+    arquive.write("Jesus is the light of the world")
 
-with open('archive.txt', 'r') as archive:
-    print(archive.read())
+with open('archive.txt', 'r') as arquive:
+    print(arquive.read())
