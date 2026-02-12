656a50419b0a38b672b8a39918af4de0d9066d6c
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-12 19:19:10 -0300

    Update 21_transform_txt_to_dict.py

--- Python/treinamentos/21_transform_txt_to_dict.py
+++ Python/treinamentos/21_transform_txt_to_dict.py
@@ -1,8 +1,1 @@
-dados = {}
 
-with open("dados.txt", "r", encoding="utf-8") as arquivo:
-    for linha in arquivo:
-        chave, valor = linha.strip().split(": ")
-        dados[chave] = valor
-
-print(dados)
