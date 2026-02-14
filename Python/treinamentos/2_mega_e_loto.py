2a69d4172d533f9894bcc5528b5d833f40ba59be
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-14 19:34:27 -0300

    Update 2_mega_e_loto.py

--- Python/treinamentos/2_mega_e_loto.py
+++ Python/treinamentos/2_mega_e_loto.py
@@ -9,14 +9,21 @@ def lotofacil():
     x = sorted(random.sample(range(1,26),15))
     print(x)
 
+def lucky_day():
+    months = ["jan", 'fev', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sept', 'octo', 'nov', 'dec']
+    x = sorted(random.sample(range(1,32),7))
+    y = random.choice(months)
+    print(x, y)
+
 while True:
-    option = int(input("Put 1 to lotofacil, 2 to megasena and 3 to exit: "))
+    option = int(input("Put 1 to lotofacil, 2 to megasena and 3 to luckyday, and 4 to exit: "))
     match option:
         case 1:
             mega()
         case 2:
             lotofacil()
         case 3:
+            lucky_day()
+        case 4:
             break
-        case _:
-            print("wrong option: ")
+            
