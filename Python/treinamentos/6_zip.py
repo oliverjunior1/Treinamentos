2e7312e1288ac17b7c4f1aca68b0bb59632d1d63
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-13 23:27:55 -0300

    Update 6_zip.py

--- Python/treinamentos/6_zip.py
+++ Python/treinamentos/6_zip.py
@@ -1,6 +1,7 @@
 list_1 = ['Name', 'Hobbie', 'Age']
 list_2 = ['Pepe', 'Jogar videogame', 12]
 
-list_union = dict(zip(list_1, list_2))
+my_dict = dict(zip(list_1, list_2))
 
-print(list_union)
+print(my_dict)
+
