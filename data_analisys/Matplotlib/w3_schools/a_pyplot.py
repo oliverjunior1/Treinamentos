40f8719057a0adbd02df06b3372d5ee4f0f9dcda
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-17 14:33:11 -0300

    Update a_pyplot.py

--- data_analisys/Matplotlib/w3_schools/a_pyplot.py
+++ data_analisys/Matplotlib/w3_schools/a_pyplot.py
@@ -1,8 +1,11 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-x = pd.array([1,9,7])
+x = pd.array([45,39,32])
+y = pd.array([10,22,49])
 
-plt.plot(x)
+plt.plot(x, color="red", label='Stalinacio')
+plt.plot(y, color='blue', label='Flávio')
+plt.legend('SF')
 
 plt.show()
