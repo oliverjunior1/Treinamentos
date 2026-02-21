f40def0fac305fff94a9b7dbfd24ca0cafb8f694
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-21 16:34:34 -0300

    Update 13_sets_basics.py

--- Python/treinamentos/code_challenge_w3/13_sets_basics.py
+++ Python/treinamentos/code_challenge_w3/13_sets_basics.py
@@ -5,11 +5,12 @@ Remove "green" from the set using discard()
 Print the number of items using len()'''
 
 # Create the set
-
+colors = {"red", "green", "blue"}
 # Print the set
-
+print(colors)
 # Add "yellow"
-
+colors.add("yellow")
 # Remove "green"
-
+colors.discard("green")
 # Print the number of items
+print(len(colors))
