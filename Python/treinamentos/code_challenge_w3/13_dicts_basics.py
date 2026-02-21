ff1484bfaf70813c3c9be8bb834f4093c93d47e8
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-21 16:25:29 -0300

    Create 13_dicts_basics.py

---
+++ Python/treinamentos/code_challenge_w3/13_dicts_basics.py
@@ -0,0 +1,15 @@
+'''Create a set called colors with the values "red", "green", "blue"
+Print the set
+Add "yellow" to the set using add()
+Remove "green" from the set using discard()
+Print the number of items using len()'''
+
+# Create the set
+
+# Print the set
+
+# Add "yellow"
+
+# Remove "green"
+
+# Print the number of items
