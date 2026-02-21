c6e8747def01127301abab62923dcfe9173fd22a
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-21 17:41:58 -0300

    Update 17_while.py

--- Python/treinamentos/code_challenge_w3/17_while.py
+++ Python/treinamentos/code_challenge_w3/17_while.py
@@ -1,14 +1,15 @@
-'''Create a variable day with the value 3
-Use a match statement to check the value of day
-Add a case 3 that prints "Wednesday"
-Add a wildcard case _ that prints "Other day"'''
+'''Create a variable i with the value 0
+Write a while loop that runs as long as i is less than 6
+Inside the loop: increment i by 1
+If i equals 3, use continue to skip that iteration
+Print i'''
 
-# Create variable day
-day = 3
-# Use match statement
+# Create the i variable
+i = 0
+# While loop: print 1-5, skip 3 with continue
+while i <6:
+    i += 1
+    if i==3:
+        continue
+    print(i)
 
-match day:
-    case 3:
-        print('Wednesday')
-    case _:
-        print('Other day')
