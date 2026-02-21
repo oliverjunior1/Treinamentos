b3a7976ec5ed70010b54facd2ffdd06e2fd0992b
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-21 16:22:02 -0300

    Update 12_tuples_basics.py

--- Python/treinamentos/code_challenge_w3/12_tuples_basics.py
+++ Python/treinamentos/code_challenge_w3/12_tuples_basics.py
@@ -5,11 +5,12 @@ Unpack the tuple into three variables a, b, c
 Print the variable b'''
 
 # Create the tuple
-
+fruits = ("apple", "banana", "cherry")
 # Print the second item
-
+print(fruits[1])
 # Print the number of items
-
+print(len(fruits))
 # Unpack the tuple
-
+a, b, c = fruits
 # Print b
+print(b)
