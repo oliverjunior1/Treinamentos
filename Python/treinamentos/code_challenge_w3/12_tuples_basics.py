c009b70571206c163ef03a4c4cecd6769ac15824
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-21 16:18:25 -0300

    Update 12_tuples_basics.py

--- Python/treinamentos/code_challenge_w3/12_tuples_basics.py
+++ Python/treinamentos/code_challenge_w3/12_tuples_basics.py
@@ -1,1 +1,15 @@
-    
+'''Create a tuple called fruits with the values "apple", "banana", "cherry"
+Print the second item in the tuple
+Print the number of items using len()
+Unpack the tuple into three variables a, b, c
+Print the variable b'''
+
+# Create the tuple
+
+# Print the second item
+
+# Print the number of items
+
+# Unpack the tuple
+
+# Print b
