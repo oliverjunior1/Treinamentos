4d3170eb6288dd3828a1680be50b64d3b1f6daa3
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-22 14:56:51 -0300

    Update 21_arrays.py

--- Python/treinamentos/code_challenge_w3/21_arrays.py
+++ Python/treinamentos/code_challenge_w3/21_arrays.py
@@ -4,9 +4,10 @@ Change the second item to "Toyota"
 Print the list'''
 
 # Create a list
-
+cars = ["Ford", "Volvo", "BMW"]
 # Print the first item
-
+print(cars[0])
 # Change the second item to "Toyota"
-
+cars[1] = "Toyota"
 # Print the list
+print(cars)
