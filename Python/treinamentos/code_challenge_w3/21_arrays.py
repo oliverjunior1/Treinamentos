c18c8f29d3055cf869b4994ef05ce7a3ecd5dd98
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-22 14:52:15 -0300

    Update 21_arrays.py

---
+++ Python/treinamentos/code_challenge_w3/21_arrays.py
@@ -0,0 +1,12 @@
+'''Create a list called cars with the values "Ford", "Volvo", "BMW"
+Print the first item in the list
+Change the second item to "Toyota"
+Print the list'''
+
+# Create a list
+
+# Print the first item
+
+# Change the second item to "Toyota"
+
+# Print the list
