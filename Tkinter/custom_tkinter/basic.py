cc8a0dafd41fb34d1f3ee5a0b7df069deb58e1a7
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-23 20:13:52 -0300

    Create basic.py

---
+++ Tkinter/custom_tkinter/basic.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode('dark')
+
+ctk.set_default_color_theme('blue')
+
+app = ctk.CTk()
+app.geometry('400x400')
+app.title('Modern Login UI using customtkinter')
+
+app.mainloop()
