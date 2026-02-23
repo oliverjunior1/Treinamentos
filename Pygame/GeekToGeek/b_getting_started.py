b89fbdc0ff6b0b93dc695adaf1865ac11549de8b
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-23 19:45:21 -0300

    Update b_getting_started.py

--- Pygame/GeekToGeek/b_getting_started.py
+++ Pygame/GeekToGeek/b_getting_started.py
@@ -1,4 +1,16 @@
+import pygame
 
+pygame.init()
+
+canvas = pygame.display.set_mode((500,500))
+pygame.display.set_caption("My board")
+exit = False
+
+while not exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+        pygame.display.update()
 
 
 
