60d13a1a5a0e1e03d03f80aa878c8a495ba28b53
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-23 20:23:13 -0300

    Update create_login_function.py

---
+++ Tkinter/custom_tkinter/create_login_function.py
@@ -0,0 +1,16 @@
+def login():
+    username = 'Geeks'
+    password = '123456'
+    new_window = ctk.CTkTopLevel(app)
+
+    new_window.title('New Window')
+    new_window.geometry('350x150')
+
+    if user_entry.get() == username and user_pass.get() == password:
+        tkmb.showinfo(title = "Login Successful",
+        message = "You have logged in Successfully")
+        ctk.CTkLabel(new_window,
+                     text="Geeks is best \"
+                          "for learning ANYTHING !!").pack()
+
+        
