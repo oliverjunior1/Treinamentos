aad74158e00c92d8b014a99b852c263a2a6773c9
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-25 20:26:00 -0300

    Update 2_mega_e_loto_lucky_day.py

--- Python/treinamentos/2_mega_e_loto_lucky_day.py
+++ Python/treinamentos/2_mega_e_loto_lucky_day.py
@@ -6,10 +6,27 @@ def mega():
     print(x)
 
 def facil():
-    x = sorted(random.sample(range(1,61),6))
+    x = sorted(random.sample(range(1,26),15))
     print(x)
 
-def mega():
-    x = sorted(random.sample(range(1,61),6))
-    print(x)
+def lucky():
+    month = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
+             'august', 'september', 'october', 'november', 'december']
+    x = sorted(random.sample(range(1,32),7))
+    y = random.choice(month)
+    print(x, y)
+
+while True:
+    option = int(input("Put 1 to lotofacil, 2 to megasena, 3 to lucky day and 4 to exit:"))
+    match option:
+        case 1:
+            mega()
+        case 2:
+            facil()
+        case 3:
+            lucky()
+        case 4:
+            break
+        case _:
+            print("Invalid number")
 
