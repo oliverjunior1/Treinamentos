85fa1e024ce46d75f4286ad7c1b17b1cf4848b5d
oliverjunior1 <oliver-junior@hotmail.com>
2026-02-25 19:57:25 -0300

    Update 2_mega_e_loto_lucky_day.py

--- Python/treinamentos/2_mega_e_loto_lucky_day.py
+++ Python/treinamentos/2_mega_e_loto_lucky_day.py
@@ -2,30 +2,14 @@ import random
 
 
 def mega():
-    x = sorted(list(random.sample(range(1,61),6)))
+    x = sorted(random.sample(range(1,61),6))
     print(x)
 
-def loto_facil():
-    x = sorted(list(random.sample(range(1,26),15)))
+def facil():
+    x = sorted(random.sample(range(1,61),6))
     print(x)
 
-def lucky_day():
-    month = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
-    x = sorted(list(random.sample(range(1,33),7)))
-    y = random.choice(month)
-    print(x, y)
-
-while True:
-    choice = int(input("Put 1 to mega sena, 2 to lotofacil, 3 to lucky day and 4 for exit: "))
-    match choice:
-        case 1:
-            mega()
-        case 2:
-            loto_facil()
-        case 3:
-            lucky_day()
-        case 4:
-            break
-        case _:
-            print('Invalid number!')
+def mega():
+    x = sorted(random.sample(range(1,61),6))
+    print(x)
 
