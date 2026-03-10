bb017c3b9c16ec710623ba104a4d112eda8b1b16
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-10 20:25:23 -0300

    Update 5_markers.py

--- Matplotlib/5_markers.py
+++ Matplotlib/5_markers.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = pd.array([1,15,3,20,6])
-y = pd.array([5,10,15,20,25])
-
-plt.plot(y, x, 'o')
-
+x = pd.array([35,45,15,25,77,99])
+y = pd.DataFrame([15,5,25,77,55,15])
+plt.plot(x, y, 'o')
 plt.show()
