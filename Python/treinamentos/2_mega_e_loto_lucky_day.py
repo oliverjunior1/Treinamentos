5187ceb78e8be2dd53ac7e907048cd4158cecfca
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-10 19:42:47 -0300

    Update 2_mega_e_loto_lucky_day.py

--- Python/treinamentos/2_mega_e_loto_lucky_day.py
+++ Python/treinamentos/2_mega_e_loto_lucky_day.py
@@ -2,31 +2,31 @@ import random
 
 
 def mega():
-    x = sorted(random.sample(range(1,61),6))
-    print(x)
+    lucky = sorted(random.sample(range(1,61),6))
+    print(lucky)
 
-def facil():
-    x = sorted(random.sample(range(1,26),15))
-    print(x)
+def lotofacil():
+    lucky = sorted(random.sample(range(1,26),15))
+    print(lucky)
 
-def lucky():
-    month = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
-             'august', 'september', 'october', 'november', 'december']
-    x = sorted(random.sample(range(1,32),7))
-    y = random.choice(month)
-    print(x, y)
+def lucky_day():
+    month = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
+             'november', 'december']
+    random_month = random.choice(month)
+    lucky = sorted(random.sample(range(1,34),7))
+    print(lucky, random_month)
 
 while True:
-    option = int(input("Put 1 to lotofacil, 2 to megasena, 3 to lucky day and 4 to exit:"))
+    option = int(input("Put 1 to megasena, 2 to lotofacil, 3 to lucky day and 4 to exit: "))
     match option:
         case 1:
             mega()
         case 2:
-            facil()
+            lotofacil()
         case 3:
-            lucky()
+            lucky_day()
         case 4:
             break
         case _:
-            print("Invalid number")
+            print("Invalid number!")
 
