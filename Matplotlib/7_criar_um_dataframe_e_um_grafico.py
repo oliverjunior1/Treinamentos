8f0ae834b39ab7b7496cfb13ac1cd2487c52af3b
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-10 21:26:49 -0300

    Update 7_criar_um_dataframe_e_um_grafico.py

--- Matplotlib/7_criar_um_dataframe_e_um_grafico.py
+++ Matplotlib/7_criar_um_dataframe_e_um_grafico.py
@@ -35,4 +35,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x = pd.DataFrame({'dia':[1,2,3,4,5],'vendas':[]})
+# Crie um DataFrame com esses dados usando pandas.
+x = pd.DataFrame({'dia':[1,2,3,4,5],'vendas':[100,150,130,170,160]})
+
+# Mostre o DataFrame na tela.
+print(x)
+
+# Gere um gráfico de linha mostrando a evolução das vendas.
+plt.plot(x['dia'], x['vendas'])
+
+# Título: "Vendas da Semana"
+plt.title("Vendas da Semana")
+
+# Nome no eixo X: "Dia"
+plt.xlabel('Dia')
+
+# Nome no eixo Y: "Vendas"
+plt.ylabel('Vendas')
+
+# Calcule a média das vendas.
+media_das_vendas = sum(x['vendas'])/len(x['vendas'])
+print(media_das_vendas)
+
+# Mostre essa média como uma linha horizontal no gráfico.
+plt.figtext(0.5,0.2,f"A media das vendas foi: {media_das_vendas}.", ha='center')
+
+plt.show()
