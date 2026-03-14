2cf8944cef4203277eef1dc4a845c5e8a623b1ef
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-13 23:18:58 -0300

    Update 11_decorations_with_arguments.py

--- Python/treinamentos/11_decorations_with_arguments.py
+++ Python/treinamentos/11_decorations_with_arguments.py
@@ -1,13 +1,14 @@
 def fun1(x):
     def fun2(*args):
-        print("#####################")
+        print("###########################")
         x(*args)
-        print("#####################")
+        print("###########################")
     return fun2
 
 @fun1
 def greetings(*args):
-    for a in args:
-        print(f"Hello {a}")
+    for arg in args:
+        print(f"Hello {arg}")
+        print("###########################")
 
-greetings("Joaquim", "Alyne", "Joao")
+greetings('Alyne', 'Pepe', 'Mari', 'Joaquim')
