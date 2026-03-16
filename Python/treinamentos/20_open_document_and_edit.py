67f0001bb9ef2d1a00478f4994e5ac0b623208ff
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-16 20:39:40 -0300

    Update 20_open_document_and_edit.py

--- Python/treinamentos/20_open_document_and_edit.py
+++
@@ -1,5 +0,0 @@
-with open('archive.txt', 'w') as archive:
-    archive.write('"Anyone go to Father unless for me", said Jesus.')
-
-with open('archive.txt', 'r') as archive:
-    print(archive.read())
