d68935a01e328b2db49f8ac532457e475fbb6b17
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-18 21:56:34 -0300

    Update 2_mega_e_loto_lucky_day.py

--- Python/treinamentos/2_mega_e_loto_lucky_day.py
+++ Python/treinamentos/2_mega_e_loto_lucky_day.py
@@ -6,10 +6,15 @@ def mega():
     print(x)
 
 def facil():
-    x = list(random.sample(range(1,61),6))
+    x = list(random.sample(range(1,26),15))
     print(x)
 
-def faluckday():
-    x = list(random.sample(range(1,61),6))
-    print(x)
+def luckday():
+    x = list(random.sample(range(1,32),7))
+    month = ['january', 'february', 'march', 'april', 'may', 'june'
+             'july', 'august', 'september', 'october', 'november', 'december']
+    print(x, random.choice(month))
+
+while True:
+    option = 
 
