8931cfde0598f7c099b2763fc52054470d77c268
oliverjunior1 <oliver-junior@hotmail.com>
2026-03-18 21:43:31 -0300

    Update 2_mega_e_loto_lucky_day.py

---
+++ Python/treinamentos/2_mega_e_loto_lucky_day.py
@@ -0,0 +1,15 @@
+import random
+
+
+def mega():
+    x = list(random.sample(range(1,61),6))
+    print(x)
+
+def facil():
+    x = list(random.sample(range(1,61),6))
+    print(x)
+
+def faluckday():
+    x = list(random.sample(range(1,61),6))
+    print(x)
+
